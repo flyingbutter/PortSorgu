b373671f3af570ab0efb4e8f68837e0c06617ecb
flyingbutter <mtanir22@gmail.com>
2017-06-15 11:47:18 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace PortSorgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(33, 172);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1277, 406);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("https://bayi.dsmart.com.tr/Web/Pages/index.aspx", System.UriKind.Absolute);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1235, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "sorgula";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(212, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(529, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(529, 99);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(157, 584);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1153, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 594);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Progress";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(445, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "PSTN NO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ADSL NO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 619);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Runtime.InteropServices;
+using Jurassic.Library;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System.Collections;
+
+namespace PortSorgu
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection Econ;
+        DataTable Exceldt;
+        string constr, Query;
+        string filePath;
+        string outputPath;
+       // private BackgroundWorker _worker;
+        string a;
+        int s;
+        int max;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private const int INTERNET_COOKIE_HTTPONLY = 0x00002000;
+        private string html_response;
+        private string CsrfKey;
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        private static extern bool InternetGetCookieEx(
+            string url,
+            string cookieName,
+            StringBuilder cookieData,
+            ref int size,
+            int flags,
+            IntPtr pReserved);
+
+        /// <summary>
+        /// Returns cookie contents as a string
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetCookie(string url)
+        {
+            int size = 512;
+            StringBuilder sb = new StringBuilder(size);
+            if (!InternetGetCookieEx(url, null, sb, ref size, INTERNET_COOKIE_HTTPONLY, IntPtr.Zero))
+            {
+                if (size < 0)
+                {
+                    return null;
+                }
+                sb = new StringBuilder(size);
+                if (!InternetGetCookieEx(url, null, sb, ref size, INTERNET_COOKIE_HTTPONLY, IntPtr.Zero))
+                {
+                    return null;
+                }
+            }
+            return sb.ToString();
+        }
+    
+
+    private void ExcelConn(string FilePath)
+        {
+
+            constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES;""", FilePath);
+            Econ = new OleDbConnection(constr);
+
+        }
+        public delegate void updatebar();
+
+        private void UpdateProgress()
+        {
+
+            progressBar1.Value += 1;
+            label1.Text = s+"/"+max;
+
+        }
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string directoryPath = Path.GetDirectoryName(filePath) + "\\temps";
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            Directory.CreateDirectory(directoryPath);
+
+            outputPath = directoryPath + "\\" + fileName + ".csv";
+
+            Query = string.Format("Select * FROM [{0}]", comboBox1.SelectedValue);
+            OleDbCommand Ecom = new OleDbCommand(Query, Econ);
+
+            Econ.Open();
+
+            DataSet ds = new DataSet();
+
+            OleDbDataAdapter oda = new OleDbDataAdapter(Query, Econ);
+            Econ.Close();
+            oda.Fill(ds);
+
+            Exceldt = ds.Tables[0];
+            max = Exceldt.Rows.Count;
+            string pstnNo="";
+            string adslNo="";
+            if (comboBox2.SelectedIndex>-1)
+            {
+                 pstnNo = comboBox2.SelectedItem.ToString();
+            }
+
+            if (comboBox3.SelectedIndex>-1)
+            {
+                adslNo = comboBox3.SelectedItem.ToString();
+            }
+
+            webBrowser1.Navigate("https://bayi.dsmart.com.tr/WEB/Pages/InternetTechFinder.aspx?menuId=-1&customerId=-1");
+             a = webBrowser1.DocumentText;
+            int startindex = 0;
+            if (File.Exists(outputPath))
+            {
+                startindex = File.ReadLines(outputPath).Count();
+              //  Console.WriteLine(startindex);
+
+            }
+            progressBar1.Maximum = Exceldt.Rows.Count;
+            progressBar1.Value = startindex;
+            try
+            {
+                await Task.Factory.StartNew(() => sorgula(pstnNo,adslNo, startindex));
+            }
+            catch (Exception r)
+            {
+
+                Console.WriteLine(r.StackTrace);
+            }   
+
+        }
+
+
+        public void sorgula(string pstnNo,string adslNo,int startIndex)
+        {
+            
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(a);
+
+            HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("/html/body");
+
+            var script = bodyNode.Descendants()
+                                         .Where(n => n.Name == "script")
+                                         .First().InnerText;
+
+            // Return the data of spect and stringify it into a proper JSON object
+            var engine = new Jurassic.ScriptEngine();
+            var result = engine.Evaluate(script);
+
+            CsrfKey = engine.GetGlobalValue("csrfKey").ToString();
+            Console.WriteLine(CsrfKey);
+
+            // var json = JSONObject.Stringify(engine, result);
+
+            //Console.WriteLine(json);
+            ArrayList cookies = new ArrayList();
+            foreach (string cookie in GetCookie("https://bayi.dsmart.com.tr/Web/Pages/index.aspx").Split(';'))
+            {
+                cookies.Add(cookie);
+            }
+    
+            for (s = startIndex; s < Exceldt.Rows.Count; s++)
+            {
+                progressBar1.Invoke(new updatebar(this.UpdateProgress));
+
+                try
+                {
+                    DataRow row = Exceldt.Rows[s];
+                    string pstn="";
+                    string adsl="";
+
+                    if (!String.IsNullOrEmpty(pstnNo))
+                    {
+                        pstn = row[pstnNo].ToString();
+
+                    }
+
+                    if (!String.IsNullOrEmpty(adslNo))
+                    {
+                        adsl = row[adslNo].ToString();
+
+                    }
+
+                    String Url = "https://bayi.dsmart.com.tr/Facade/DealerService.asmx/GetAdslInfrastructure";
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                    request.CookieContainer = new CookieContainer();
+                    request.Referer = "https://bayi.dsmart.com.tr/WEB/Pages/InternetTechFinder.aspx?menuId=-1&customerId=-1";
+                    request.Host = "bayi.dsmart.com.tr";
+                    request.KeepAlive = true;
+                    request.Method = "POST";
+                    request.Headers.Add("CsrfKey", CsrfKey);
+                    request.Accept = "*/*";
+                    request.Headers.Add("X-Requested-With", "XMLHttpRequest");
+                    request.Headers.Add("Origin", "https://bayi.dsmart.com.tr");
+                    request.ContentType = "application/json; charset=UTF-8";
+
+                    // 
+                    foreach (string cookie in cookies)
+                    {
+                        Cookie mycook = new Cookie(cookie.Split('=')[0].Replace(" ", ""), cookie.Split('=')[1]) { Domain = "bayi.dsmart.com.tr" };
+                        request.CookieContainer.Add(mycook);
+                    }
+
+
+
+
+                    var postData = "{\"bbkquery\":\"\",\"pstnquery\":\"" + pstn + "\",\"xdslquery\":\""+adsl+"\"}";
+
+                    var data = Encoding.ASCII.GetBytes(postData);
+                    request.ContentLength = data.Length;
+                    using (var stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+
+
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    html_response = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                    RootObjectMain r = JsonConvert.DeserializeObject<RootObjectMain>(html_response);
+
+                    RootObject t = JsonConvert.DeserializeObject<RootObject>(r.d.Substring(3));
+
+                   // Console.WriteLine(t.CurrentIsp);
+                    string isp = t.CurrentIsp;
+
+                    using (StreamWriter w = File.AppendText(outputPath))
+                    {
+                        w.WriteLine(pstn + "," + isp);
+                    }
+                 //   Console.Write("  " + s);
+                   
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+        }
+        public class RootObjectMain
+        {
+            public string d { get; set; }
+        }
+        public class FoundTechList
+        {
+            public string MaxSpeed { get; set; }
+            public bool NdslAvailable { get; set; }
+            public string GreenBrown { get; set; }
+            public int DistaneToSwitchboard { get; set; }
+            public string Tech { get; set; }
+            public bool PortAvailable { get; set; }
+            public string Description { get; set; }
+            public int Provider { get; set; }
+            public string RawResult { get; set; }
+        }
+
+        public class InputQueryList
+        {
+            public int Provider { get; set; }
+            public int InputType { get; set; }
+            public string Value { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<FoundTechList> FoundTechList { get; set; }
+            public string CurrentIsp { get; set; }
+            public string SaleType { get; set; }
+            public string SaleTypeExplanation { get; set; }
+            public bool ForceNakedTransfer { get; set; }
+            public bool ForceNakedAdsl { get; set; }
+            public bool ForbidNakedAdsl { get; set; }
+            public List<InputQueryList> InputQueryList { get; set; }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ExcelConn(filePath);
+
+
+            Query = string.Format("Select * FROM [{0}]", comboBox1.SelectedValue);
+            OleDbCommand Ecom = new OleDbCommand(Query, Econ);
+
+            Econ.Open();
+
+            DataSet ds = new DataSet();
+
+            OleDbDataAdapter oda = new OleDbDataAdapter(Query, Econ);
+            Econ.Close();
+            oda.Fill(ds);
+            Exceldt = ds.Tables[0];
+
+            foreach (DataColumn column in Exceldt.Columns)
+            {
+
+                comboBox2.Items.Add(column.ColumnName);
+                comboBox3.Items.Add(column.ColumnName);
+
+
+
+            }
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Excel files (*.xls,*.xlsx)|*.xls;*.xlsx";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            filePath = openFileDialog1.FileName;
+
+                            ExcelConn(filePath);
+                            Econ.Open();
+
+                            var ExcelTables = Econ.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, new Object[] { null, null, null, "TABLE" });
+                            comboBox1.DisplayMember = "TABLE_NAME";
+                            comboBox1.ValueMember = "TABLE_NAME";
+                            comboBox1.DataSource = ExcelTables;
+
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
